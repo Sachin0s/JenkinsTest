81cbd39ef43b50c8f7f6cda1378fa14888452ad2
Sachin <rnd.sachin@gmail.com>
2020-03-19 13:01:24 +0530

    First time commit

---
+++ test1.bat
@@ -0,0 +1,4 @@
+start cmd.exe \k
+cd C:\Program Files (x86)\UiPath\Studio
+UiRobot -pack "C:\Users\SachinBhatnagar\Documents\UiPath\JenkingsTest\project.json" -o "C:\Users\SachinBhatnagar\Documents\UiPath\JenkingsTest" 
+exit
